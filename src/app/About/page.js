c2c532ec95fa9ee49c210d8bbfb5f94de9fcf0b4
naufalrafi-hub <rafi.harahaps@gmail.com>
2023-05-26 05:23:56 +0700

    adding information on blank pages

---
+++ src/app/About/page.js
@@ -0,0 +1,10 @@
+export default function About() {
+  
+    return (
+        <div className="mt-300">
+            <h2 className="text-center">
+            COMING REALLY SOON
+            </h2>
+        </div>
+    )
+}
