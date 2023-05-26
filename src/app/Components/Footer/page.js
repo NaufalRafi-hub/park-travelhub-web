5880cfa173a6af804771802d3e194b72f6a0f276
naufalrafi-hub <rafi.harahaps@gmail.com>
2023-05-26 13:09:21 +0700

    update footer

--- src/app/Components/Footer/page.js
+++ src/app/Components/Footer/page.js
@@ -1,10 +1,24 @@
+import Image from "next/image";
+
 export default function Footer() {
   
     return (
-        <div className="mt-300">
-            <h2 className="text-center">
-            PARK TRAVEL HUB Sport is an established and trusted bespoke sports travel company with nearly 25 years’ experience in providing sports packages for individual clients, corporates, concierge and incentive agencies and the England Football Media. We provide tailor-made packages for Formula 1, rugby, tennis, golf, football and countless other sporting events both domestically and worldwide.
-            </h2>
+        <div className="grid grid-cols-3">
+            <div className="col-span-2 bg-greenie">
+                <h1 className="italic font-bold w-12 mb-20 color-greenie">
+                    CONTACT US!
+                </h1>
+            </div>
+            <div className="flex flex-col align-center p-12 bg-grayie">
+                <Image className="ml-auto mr-auto" src='/park-logo.png' alt='park-logo' width={100} height={100} />
+                <div>
+                    <a className="text-white" href="">HOME</a>
+                    <a className="text-white" href="">ABOUT US</a>
+                    <a className="text-white" href="">TOUR & TRAVEL</a>
+                    <a className="text-white" href="">SOCIALS</a>
+                </div>
+            </div>
+            
         </div>
     )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from './Components/Footer/page'
 import Navbar from './Components/Navbar/page'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Navbar />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   )
 }
