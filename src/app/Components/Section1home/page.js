e57c09140c11a54333f3b1b6d54cc849d26ea49b
naufalrafi-hub <rafi.harahaps@gmail.com>
2023-05-30 03:05:32 +0700

    adding framer motion and add some header on landing page

--- src/app/Components/Section1home/page.js
+++ src/app/Components/Section1home/page.js
@@ -1,8 +1,40 @@
+import Image from 'next/image'
+import './Sec1.css'
+
 export default function Section1home() {
   
     return (
-        <div className="mt-300">
-            slider
+        <div className="mt-[147px] w-full relative">
+            <div className='absolute top-1 left-5'>
+                        <Image src='/park-s-header.png'
+                        width={0}
+                        height={0}
+                        sizes='100vh'
+                        style={{
+                            width: '400px',
+                            height: '571px'
+                        }}
+                        />
+                        
+                    </div>
+            <div className="bg-sec1 flex p-24">
+                <div className='wrap-1-sec1 flex flex-col gap-14 relative'>
+                    <Image src='/park-s-logo.png'
+                    width={0}
+                    height={0}
+                    sizes='100vh'
+                    style={{width: '55px', height: '79px'}}
+                    />
+                    <h1 className='font-bold text-white'>HI SPORT ENTHUSIAST </h1>
+                    <div className='bg-greenie p-4 font-bold text-center'>
+                        <a>
+                            TRAVEL WITH US
+                        </a>
+                    </div>
+                    
+                    
+                </div>
+            </div>
         </div>
     )
 }
