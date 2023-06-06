a4c456ae9fe2264f430046a3dd2b0220f23d31c5
naufalrafi-hub <rafi.harahaps@gmail.com>
2023-06-07 01:29:31 +0700

    adding link to sports section

--- src/app/Components/KeenSlider/page.js
+++ src/app/Components/KeenSlider/page.js
@@ -2,6 +2,7 @@ import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import "./keenCSS.css"
 import Image from "next/image";
+import Link from 'next/link';
 
 export default () => {
   const [sliderRef] = useKeenSlider({
@@ -18,6 +19,7 @@ export default () => {
   return (
     <div ref={sliderRef} className="keen-slider mt-180">
       <div className="keen-slider__slide number-slide1" style={{maxWidth: 250, minWidth: 250}}>
+      <Link href="/golf">
             <div className="w-[302px] h-[500px] relative">
                 <Image src='/golf.png' width="0"
                     height="0"
@@ -25,8 +27,10 @@ export default () => {
                     style={{ width: '302px', height: '470px', minWidth: '302px', minHeight: '470px' }} />
                 <h3 className="font-bold absolute left-2 bottom-0 text-left w-[108px]">GOLF TRIP</h3>
             </div>
+      </Link>
       </div>
       <div className="keen-slider__slide number-slide2" style={{maxWidth: 250, minWidth: 250}}>
+        <Link href="/cycling">
             <div className="w-[302px] h-[500px] relative">
                 <Image src='/biking.png' 
                     width="0"
@@ -35,8 +39,10 @@ export default () => {
                     style={{ width: '302px', height: '470px' }} /> 
                 <h3 className="font-bold absolute left-2 bottom-0 text-left w-[208px]">CYCLING TRIP</h3>
             </div>
+          </Link>
       </div>
       <div className="keen-slider__slide number-slide3" style={{maxWidth: 250, minWidth: 250}}>
+        <Link href="/yoga"> 
             <div className="w-[302px] h-[500px] relative">
                 <Image src='/yoga.png' width="0"
                     height="0"
@@ -44,8 +50,10 @@ export default () => {
                     style={{ width: '302px', height: '470px' }} /> 
                 <h3 className="font-bold absolute left-2 bottom-0 text-left w-[208px]">YOGA TRIP</h3>
             </div>
+          </Link>
       </div>
       <div className="keen-slider__slide number-slide4" style={{maxWidth: 250, minWidth: 250}}>
+        <Link href="/tennis">
             <div className="w-[302px] h-[500px] relative">
                 <Image src='/tennis.png' 
                     width="0"
@@ -54,14 +62,17 @@ export default () => {
                     style={{ width: '302px', height: '470px' }} /> 
                 <h3 className="font-bold absolute left-2 bottom-0 text-left w-[208px]">TENNIS TRIP</h3>
             </div>
+          </Link>
       </div>
       <div className="keen-slider__slide number-slide5" style={{maxWidth: 250, minWidth: 250}}>
+        <Link href="/golf">
             <div className="bg-greenie w-[250px] h-[500px] flex flex-col justify-center items-center">
                 <Image src='/add_icon.png' width={100} height={0}
                 style={{ marginLeft:'auto', marginRight:'auto' }}
                 />
                 <h3 className="font-bold mt-4 text-center p-10">REQUEST CUSTOM TRIP</h3>
             </div>
+          </Link>
       </div>
     </div>
   )
---
+++ src/app/yoga/page.js
@@ -0,0 +1,10 @@
+export default function Yoga() {
+  
+    return (
+        <div className="mt-300">
+            <h2 className="text-center">
+            PARK TRAVEL HUB Sport is an established and trusted bespoke sports travel company with nearly 25 years’ experience in providing sports packages for individual clients, corporates, concierge and incentive agencies and the England Football Media. We provide tailor-made packages for Formula 1, rugby, tennis, golf, football and countless other sporting events both domestically and worldwide.
+            </h2>
+        </div>
+    )
+}
