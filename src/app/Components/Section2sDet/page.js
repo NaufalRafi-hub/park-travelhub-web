00d6d14861e566f4053221e26b1a07d873f07f7b
naufalrafi-hub <rafi.harahaps@gmail.com>
2023-06-07 15:58:40 +0700

    add section2 on sDet

---
+++ src/app/Components/Section2sDet/page.js
@@ -0,0 +1,18 @@
+import './styles.css'
+export default function Section2sDet() {
+  
+    return (
+        <div className='bg-grayie'>
+            <div className='p-24 mt-180 flex'>
+
+                <h1 className="italic font-bold w-[160px] mb-20 color-greenie">
+                    SELECT CITY
+                </h1>
+                <h1 className="italic font-bold w-[160px] mb-20 color-greenie">
+                    PACKAGES
+                </h1>
+            </div>
+        </div>
+        
+    )
+}
--- src/app/golf/doer/page.js
+++ src/app/golf/doer/page.js
@@ -1,11 +1,15 @@
 // import './styles.css'
-import Section1sDet from '../../Components/Section1sDet/page'
-// import Section1sports from '../Components/Section1sports/page'
-// import Section2sports from '../Components/Section2sports/page'
+import Section1sDet from '../../Components/section1sDet/page'
+import Section2sDet from '../../Components/Section2sDet/page'
+
 
 export default function Doer() {
   
     return (
-        <Section1sDet />
+        <div>
+            <Section1sDet />
+            <Section2sDet />
+        </div>
+        
     )
 }
