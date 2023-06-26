ca436e28cce80cde07ab5725678a35978c41444e
naufalrafi-hub <rafi.harahaps@gmail.com>
2023-06-27 00:19:17 +0700

    edit section s2det

--- src/app/Components/Section2sDet/page.js
+++ src/app/Components/Section2sDet/page.js
@@ -1,6 +1,10 @@
+"use client"
+import { useState } from 'react';
 import './styles.css'
 export default function Section2sDet(props) {
     const {data} = props 
+    const [idx, setIdx] = useState(0)
+    const [content, setContent] = useState(data[0].city[idx].place);
     console.log (data[0].city[0].place)
     return (
         <div className='bg-grayie'>
@@ -12,7 +16,14 @@ export default function Section2sDet(props) {
                 
                     {data[0].city.map((item, index) => (
                     <>
-                        <h3 className='text-white' key={index}>{item.place}</h3>
+                        <h3 className='text-white' 
+                        key={index}
+                        onClick={() => {
+                            setContent(item.place);
+                            setIdx(index);
+                          }}>
+                            {item.place}
+                        </h3>
                     </>
                     ))}
                 </div>
